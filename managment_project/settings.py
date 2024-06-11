1223bbc74694124db365e7eda40ae14209009efb
georgetoloraia <gtoloraia19@gmail.com>
2024-06-11 21:47:51 +0400

    add: [x] DRF and managment_app in django setings

--- managment_project/settings.py
+++ managment_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed
+    'rest_framework',
+    'managment',
 ]
 
 MIDDLEWARE = [
